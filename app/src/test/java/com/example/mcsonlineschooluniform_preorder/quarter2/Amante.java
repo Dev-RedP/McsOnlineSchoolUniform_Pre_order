70265cfe4932bb0070fac593713348d72bf48e35
ellah245 <ellahamante3@gmail.com>
2026-09-17 12:33:40 +0800

    final

--- app/src/test/java/com/example/mcsonlineschooluniform_preorder/quarter2/Amante.java
+++ app/src/test/java/com/example/mcsonlineschooluniform_preorder/quarter2/Amante.java
@@ -1,4 +1,18 @@
 package com.example.mcsonlineschooluniform_preorder.quarter2;
 
+import org.junit.Test;
+
 public class Amante {
+    @Test
+    public void printMyProfile() {
+        String myName = "MICHA";
+        String petName = "BROWNIE";
+        String favFood = "SINIGANG";
+        int myAge = 17;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+    }
 }
